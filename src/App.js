45c2b1070490bdfe5398717ae01ca65511ca9cb3
yunsungjoong <yun_sj94@naver.com>
2024-03-01 17:38:40 +0900

    수정

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import TodoTemplate from './components/TodoTemplate';
+
+
+function App() {
+  return (
+  
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList />
+    </TodoTemplate>
+  )
+}
+
+export default App;
+  
---
+++ src/components/TodoListItem.js
@@ -0,0 +1,26 @@
+import { 
+    MdCheckBox,
+    MdCheckBoxOutlineBlank, 
+    MdRemoveCircleOutline,
+} from 'react-icons/md';
+import cn from 'classnames';
+import './TodoListItem.scss'
+
+
+const TodoListItem = ({todo}) => {
+    const { text, checked } = todo;
+    return (
+        <div className='TodoListItem'>
+            <div className={cn('checkbox', { checked})}>
+                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                <div className='text'>{text} </div>
+            </div>
+            <div className='remove'>
+                <MdRemoveCircleOutline />
+                
+            </div>
+        </div>
+    );
+};
+
+export default TodoListItem;
