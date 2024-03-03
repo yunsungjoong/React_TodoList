c8afc878f0123571d916e145ee6af1e1f79bea4e
yunsungjoong <yun_sj94@naver.com>
2024-03-03 23:58:50 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -6,13 +6,26 @@ import TodoTemplate from './components/TodoTemplate';
 
 function App() {
   const [todos, setTodos] = useState([
-    {}
-  ])
+    {
+      id: 1,
+      text: '리엑트 기초 알아보기',
+      checked: false,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
   return (
-  
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   )
 }
