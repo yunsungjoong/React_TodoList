9a9454a0165b1c96cd6c23ab3602106a35d1ffe9
yunsungjoong <yun_sj94@naver.com>
2024-03-04 21:43:44 +0900

    onInsert

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -3,20 +3,36 @@ import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
     const [value, setValue] = useState('');
     
     const onChange = useCallback(e => {
         setValue(e.target.value);
     }, []);
+
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value); // onInsert에 value 값을 파라미더로 넣어서 호출한다.
+            setValue('') // value 값 초기화 
+            e.preventDefault(); // submit을 하면 새로고침하기에 이를 방지하기 이 함수를 호출한다.
+        },
+        [onInsert, value],
+    );
+
+    // const onClick = useCallback(
+    //     () => {
+    //         onInsert(value);
+    //         setValue(''); // value 값 초기화
+    //     }, [onInsert, value]
+    // )
     return (
-        <form className='TodoInsert'>
+        <form className='TodoInsert' onSubmit={onSubmit}>
             <input 
                 placeholder='할 일을 입력하세요' 
                 value={value}
                 onChange={onChange}
             />
-            <button type='submit'>
+            <button>
                 <MdAdd />
             </button>
         </form>
