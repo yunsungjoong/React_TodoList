0675ca339ef1956c0b0bb545e29485fbce116f74
yunsungjoong <yun_sj94@naver.com>
2024-03-07 00:44:48 +0900

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useCallback, useRef, useState } from 'react';
+
+import { useCallback, useRef, useReducer } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
@@ -15,42 +16,55 @@ function createBulkTodos() {
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch(action.type) {
+    case 'INSERT': // 새로추가
+      // { type : 'INSERT', todo: { id: 1, text: 'todo', checked: false}}
+      return todos.concat(action.todo);
+    case 'REMOVE': // 제거
+      // { typee REMOVE', id: 1 }
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE' : // 토글
+      // { type: 'REMOVE', id: 1 }
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+        );
+        default:
+          return todos;
+  }
+}
 
-function App() {
-  const [todos, setTodos] = useState(createBulkTodos);
+
+  const App = () => {
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
   // 고유값으로 사용될 id
   // ref를 사용하여 변수 담기
 
-  const nextId = useRef(2501);
+  const nextId = useRef(4);
 
-  const onInsert = useCallback(
-    text => {
+  const onInsert = useCallback(text => {
       const todo = {
         id: nextId.current,
         text,
         checked: false,
       };
-      setTodos(todos.concat(todo)); // concat()은 배열에 요소를 추가하고 새로운 배열을 반환
+      dispatch({ type: 'INSERT', todo})
       nextId.current += 1; // nextId 현재 값에 1 증가
     },
-    [todos],
+    [],
   );
 
   const onRemove = useCallback(
     id => { 
-      setTodos(todos.filter(todo => todo.id !== id));
-    }, [todos],
+      dispatch({ type: 'REMOVE', id})
+    }, [],
   );
 
   const onToggle = useCallback(
     id => {
-    setTodos(
-      todos.map(todo => 
-        todo.id === id ? { ...todo, checked: !todo.checked} : todo, 
-        ),
-      );
-    }, [todos],
+      dispatch({ type: 'TOGGLE', id})
+    }, [],
   );
 
   return (
